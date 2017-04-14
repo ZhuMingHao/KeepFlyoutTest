74c6f4a15eb6c5d596213c6a7159d93b9b9d4bee
Nico Zhu (Shang Hai Wei Chuang Ruan Jian) <v-zhumin@microsoft.com>
2017-04-14 18:13:04 +0800

    Add project files.

---
+++ KeepFlyoutTest/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace KeepFlyoutTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+       
+        public MainPage()
+        {
+            this.InitializeComponent();
+   
+        }
+
+        private bool manual = false;
+
+        private void flyout_Closing(FlyoutBase sender, FlyoutBaseClosingEventArgs args)
+        {
+            if(manual == true)
+            {
+                args.Cancel = false;
+            }
+            else
+            {
+                args.Cancel = true;
+             
+            }
+            manual = false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            manual = true;
+            flyout.Hide();
+        }
+    }
+}
